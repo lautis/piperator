6d8440f1dc483250b424202d6fd39e7a3117ab72
Ville Lautanala <lautis@gmail.com>
2019-10-13 11:28:56 +0300

    Document Piperator.build

--- lib/piperator.rb
+++ lib/piperator.rb
@@ -5,9 +5,22 @@ require 'piperator/builder'
 
 # Top-level shortcuts
 module Piperator
-  # Build a new pipeline using DSL
+  # Build a new pipeline using DSL. This enables easy control of the pipeline
+  # stucture.
   #
-  # @see Piperator::Pipeline
+  #   Piperator.build do
+  #     wrap [1, 2, 3]
+  #     pipe(-> (enumerable) { enumerable.map { |i| i + 1 } })
+  #   end
+  #   # => Pipeline that returns [2, 3, 4] called
+  #
+  #   # Alternatively, the Builder is also given as argument to the block
+  #   Piperator.build do |p|
+  #     p.wrap [1, 2, 3]
+  #     p.pipe(-> (enumerable) { enumerable.map { |i| i + 1 } })
+  #   end
+  #   # This is similar, but allows access to instance variables.
+  #
   # @return [Pipeline] Pipeline containing defined steps
   def self.build(&block)
     return Pipeline.new unless block_given?
@@ -24,7 +37,7 @@ module Piperator
   # Build a new pipeline from a callable or an enumerable object
   #
   # @see Piperator::Pipeline.pipe
-  # @param callable An object responding to call(enumerable)
+  # @param enumerable An object responding to call(enumerable)
   # @return [Pipeline] A pipeline containing only the callable
   def self.pipe(enumerable)
     Pipeline.pipe(enumerable)
