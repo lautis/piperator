15d69cd01a7abeef430561e29312790383c52674
Ville Lautanala <lautis@gmail.com>
2022-06-14 21:20:38 +0300

    Run simplecov only on Ruby 2.5 and newer

--- piperator.gemspec
+++ piperator.gemspec
@@ -26,6 +26,4 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'bundler', '>= 1.14'
   spec.add_development_dependency 'rake', '~> 12.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
-  spec.add_development_dependency 'simplecov', '~> 0.21.2'
-  spec.add_development_dependency 'simplecov-cobertura', '~> 2.1.0'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,15 @@
 require 'bundler/setup'
-require 'simplecov'
-require 'simplecov-cobertura'
-SimpleCov.start do
-  add_filter '/spec/'
-end
 
-if ENV['CI']
-  SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+if RUBY_VERSION >= '2.5'
+  require 'simplecov'
+  require 'simplecov-cobertura'
+  SimpleCov.start do
+    add_filter '/spec/'
+  end
+
+  if ENV['CI']
+    SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+  end
 end
 
 require 'piperator'
